c210ff2372181a792df074706d5488dae95ee805
Crowdbotics <team@crowdbotics.com>
2021-08-03 16:42:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "krick_reactnative_29406Identifier",
+  url: "https://krick_reactnative_29406Identifier.botics.co",
+  api: "https://krick_reactnative_29406Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
